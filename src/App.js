67d64d6cbd4a26a68273489d67c3eda58a8a8eb0
Benjamin Apelman <beap9735@colorado.edu>
2024-06-10 14:54:05 -0600

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import './Pages_CSS/App.css'
+import Navbar from './Components/NavBar';
+import ProblemTable from './Components/ProblemTable';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar /> {/* Render the Navbar component */}
+      <ProblemTable/>
     </div>
   );
 }
